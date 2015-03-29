2224d39b7ba02be2e08c5cd2589fd4fd244d4767
Arnaud Casteigts <arnaud.casteigts@labri.fr>
2015-03-29 09:22:17 +0200

    Point3D lifted from JavaFX (Oracle 7 has it already, but OpenJDK 7 does not).

--- jbotsim/Node.java
+++ jbotsim/Node.java
@@ -16,7 +16,6 @@ import java.awt.geom.Point2D;
 import java.util.*;
 import java.util.List;
 
-import javafx.geometry.Point3D;
 import jbotsim.event.ClockListener;
 import jbotsim.event.MovementListener;
 
