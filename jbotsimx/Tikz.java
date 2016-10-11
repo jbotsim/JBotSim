0016769d95b6ed7dd5c2ddca3fcc47989a4f0edc
Arnaud Casteigts <arnaud.casteigts@labri.fr>
2016-10-11 15:24:39 +0200

    Support for link width and color in TikZ exportation

--- jbotsimx/Tikz.java
+++ jbotsimx/Tikz.java
@@ -15,7 +15,39 @@ import jbotsim.Link;
 import jbotsim.Node;
 import jbotsim.Topology;
 
+import java.awt.*;
+
 public class Tikz {
+	public static String getStringColor(Color color){
+		String result = "";
+		if (color == Color.black)
+			result = "black";
+		if (color == Color.red)
+			result = "red";
+		if (color == Color.blue)
+			result = "blue";
+		if (color == Color.green)
+			result = "green";
+		if (color == Color.white)
+			result = "white";
+		if (color == Color.gray)
+			result = "gray";
+		if (color == Color.cyan)
+			result = "cyan";
+		if (color == Color.magenta)
+			result = "magenta";
+		if (color == Color.orange)
+			result = "orange";
+		if (color == Color.darkGray)
+			result = "darkgray";
+		if (color == Color.lightGray)
+			result = "lightGray";
+		if (color == Color.pink)
+			result = "pink";
+		if (color == Color.yellow)
+			result = "yellow";
+		return result;
+	}
 	public static String exportTopology(Topology tp){
 		return exportTopology(tp,50);
 	}
@@ -28,7 +60,7 @@ public class Tikz {
 			for (Node n : tp.getNodes()){
 				double x=Math.round(n.getX()*100/scale)/100.0;
 				double y=Math.round((600-n.getY())*100/scale)/100.0;
-				s=s+"  \\path ("+x+","+y+") node (v" + n + ") {};"+delim;
+				s=s+"  \\path ("+x+","+y+") node ["+getStringColor(n.getColor())+"] (v" + n + ") {};"+delim;
 			}
 		}
 		s=s+"  \\tikzstyle{every node}=[draw,circle,fill=gray,inner sep=1.5]"+delim;
@@ -36,16 +68,17 @@ public class Tikz {
 			String id = "v"+n.toString();
 			double x=Math.round(n.getX()*100/scale)/100.0;
 			double y=Math.round((600-n.getY())*100/scale)/100.0;
-			s=s+"  \\path ("+x+","+y+") node ("+id+") {};"+delim;
+			String color = (n.getColor()!=null)?"["+n.getColor().toString()+"]":"";
+			s=s+"  \\path ("+x+","+y+") node ["+getStringColor(n.getColor())+"] ("+id+") {};"+delim;
 		}
 		s+="  \\tikzstyle{every path}=[];"+delim;
 		for (Link l : tp.getLinks()) {
 			String width="";
 			if (l.getWidth()>1)
-				width="[ultra thick]";
+				width=",ultra thick";
 			String id1 = "v"+l.source.toString();
 			String id2 = "v"+l.destination.toString();
-			s += "  \\draw"+width+" (" + id1 + ")--(" + id2 + ");" + delim;
+			s += "  \\draw ["+getStringColor(l.getColor())+width+"] (" + id1 + ")--(" + id2 + ");" + delim;
 		}
 		s+="\\end{tikzpicture}"+delim;
 		return s;		
