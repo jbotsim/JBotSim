fdebdb5b3d66c287cdaeb9f0d78d23e433b27844
Arnaud Casteigts <arnaud.casteigts@labri.fr>
2017-03-14 10:48:19 +0100

    Toroidal link resolver (and painter)

---
+++ src/jbotsimx/geometry/toroidal/ToroidalLinkResolver.java
@@ -0,0 +1,30 @@
+package jbotsimx.geometry.toroidal;
+
+
+import jbotsim.LinkResolver;
+import jbotsim.Node;
+
+import java.awt.geom.Point2D;
+
+/**
+ * A new type of link resolver based on the toroidal distance between nodes.
+ * Here, coordinates are assumed to be 2D (only) and nodes to be wireless.
+ */
+
+public class ToroidalLinkResolver extends LinkResolver {
+	public double toroidalDistance(Node n1, Node n2){
+		int width = n1.getTopology().getWidth();
+		int height = n1.getTopology().getHeight();
+		Point2D p1 = n1.getLocation();
+		Point2D p2 = n2.getLocation();
+		double distX = Math.abs((p1.getX() - p2.getX()));
+		distX = Math.min(distX, width - distX);
+		double distY = Math.abs((p1.getY() - p2.getY()));
+		distY = Math.min(distY, height - distY);
+		return Math.sqrt(distX*distX + distY*distY);
+	}
+	@Override
+	public boolean isHeardBy(Node n1, Node n2) {
+		return (toroidalDistance(n1, n2) < n1.getCommunicationRange());
+	}
+}
---
+++ src/jbotsimx/geometry/toroidal/ToroidalTest.java
@@ -0,0 +1,18 @@
+package jbotsimx.geometry.toroidal;
+
+import jbotsim.Topology;
+import jbotsim.ui.JViewer;
+
+/**
+ * Created by acasteig on 30/08/16.
+ */
+public class ToroidalTest {
+    public static void main(String[] args) {
+        Topology tp = new Topology();
+        tp.setLinkResolver(new ToroidalLinkResolver());
+        JViewer jv = new JViewer(tp);
+        jv.getJTopology().setLinkPainter(new ToroidalLinkPainter());
+        for (int i = 0; i < 10; i++)
+            tp.addNode(-1, -1);
+    }
+}
