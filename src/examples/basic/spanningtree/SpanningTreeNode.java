f2e30bf587183ab943dfdcced7a1ce25f639f778
Arnaud Casteigts <arnaud.casteigts@labri.fr>
2017-03-15 21:24:15 +0100

    Spanning tree example

---
+++ src/examples/basic/spanningtree/SpanningTreeNode.java
@@ -0,0 +1,33 @@
+package examples.basic.spanningtree;
+
+import jbotsim.Message;
+import jbotsim.Node;
+
+import java.awt.*;
+
+/**
+ * Created by acasteig on 10/03/15.
+ */
+public class SpanningTreeNode extends Node{
+    Node parent;
+
+    @Override
+    public void onStart() {
+        parent = null;
+    }
+
+    @Override
+    public void onSelection() {
+        parent = this;
+        sendAll(new Message());
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        if ( parent == null ){
+            parent = message.getSender();
+            getCommonLinkWith(parent).setWidth(4);
+            sendAll(new Message());
+        }
+    }
+}
