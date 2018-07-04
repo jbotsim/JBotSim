7ebe38b84eb8c2b4a87f7c85e61ad42e0e8736b4
Gerald Point <gerald.point@labri.fr>
2018-07-04 14:17:22 +0200

    add new tests

--- tests/jbotsimx/xml/ParserTest.java
+++ tests/jbotsimx/xml/ParserTest.java
@@ -2,7 +2,7 @@ package jbotsimx.xml;
 
 import jbotsim.Topology;
 import org.hamcrest.core.IsInstanceOf;
-import org.hamcrest.core.IsNull;
+import org.hamcrest.core.StringContains;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -20,26 +20,72 @@ public class ParserTest {
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void empty() {
+    public void emptyFileTest() {
         try {
-            loadXMLFile("empty.xml");
+            loadXMLFile("empty-file.xml");
         } catch (XMLTopologyParser.ParserException e) {
-            assertThat(e.getCause(), new IsInstanceOf(SAXParseException.class));
+            assertCauseParserExceptionIs(e, SAXParseException.class);
         }
     }
 
     @Test
     public void missingXMLVersionNumber() {
-        try {
-            loadXMLFile("missing-version.xml");
-        } catch (XMLTopologyParser.ParserException e) {
-            assertThat(e.getCause(), new IsNull<>());
-        }
+        missingAttribute("missing-version.xml");
     }
 
     @Test
     public void twonodes() throws XMLTopologyParser.ParserException {
         Topology T = loadXMLFile("twonodes.xml");
+        assertEquals(T.getNodes().size(), 2);
+        assertEquals(T.getLinks().size(), 1);
+    }
+
+    @Test
+    public void missingSrcTest() {
+        missingAttribute("missing-src.xml");
+    }
+
+    @Test
+    public void missingDstTest() {
+        missingAttribute("missing-dst.xml");
+    }
+
+    @Test
+    public void missingGraphTest() {
+        testXSDValidationError("missing-graph.xml", "cvc-complex-type.2.4.b");
+    }
+
+    @Test
+    public void missingClassesTest() {
+        testXSDValidationError("missing-classes.xml", "cvc-complex-type.2.4.a");
+    }
+
+    @Test
+    public void emptyGraphTest() throws XMLTopologyParser.ParserException {
+        loadXMLFile("empty-graph.xml");
+    }
+
+    @Test
+    public void incompleteModel1Test() {
+        missingAttribute("incomplete-model-class-1.xml");
+    }
+
+    @Test
+    public void incompleteModel2Test() {
+        missingAttribute("incomplete-model-class-2.xml");
+    }
+
+    @Test
+    public void incompleteModel3Test() {
+        try {
+            loadXMLFile("incomplete-model-class-3.xml");
+        } catch (XMLTopologyParser.ParserException e) {
+            assertCauseParserExceptionIs(e, ClassNotFoundException.class, "nosuchclass");
+        }
+    }
+
+    private void missingAttribute(String xmlFileName) {
+        testXSDValidationError(xmlFileName, "cvc-complex-type.4");
     }
 
     private Topology loadXMLFile(String xmlFileName) throws XMLTopologyParser.ParserException {
@@ -53,5 +99,28 @@ public class ParserTest {
         tpp.parse(is);
 
         return T;
+    }
+
+    private void testXSDValidationError(String filename, String error) {
+        try {
+            loadXMLFile(filename);
+            thrown.expect(XMLTopologyParser.ParserException.class);
+        } catch (XMLTopologyParser.ParserException e) {
+            assertXSDValidationError(e, error);
+        }
+    }
+
+    private void assertXSDValidationError(XMLTopologyParser.ParserException e, String error) {
+        assertThat(e.getMessage(), StringContains.containsString("XSD validation error: "+error+" :"));
+    }
+
+    private void assertCauseParserExceptionIs(XMLTopologyParser.ParserException e, Class c) {
+        assertThat(e.getCause(), IsInstanceOf.instanceOf(c));
+    }
+
+    private void assertCauseParserExceptionIs(XMLTopologyParser.ParserException e, Class c, String error) {
+        assertNotNull("This is a forwarded exception", e.getCause());
+        assertCauseParserExceptionIs(e,c);
+        assertThat(e.getCause().getMessage(), StringContains.containsString(error));
     }
 }
