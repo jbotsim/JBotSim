3b2b5bc0a6ffea65671929b8ac2a547c46e55240
Remi Laplace <remi.laplace@labri.fr>
2018-10-10 16:19:04 +0200

    Fix: add first unit test class (to ensure feasability) + fix LinkResolver behavior on boundaries

--- lib/jbotsim-core/src/main/java/jbotsim/LinkResolver.java
+++ lib/jbotsim-core/src/main/java/jbotsim/LinkResolver.java
@@ -15,6 +15,6 @@ package jbotsim;
 public class LinkResolver {
     public boolean isHeardBy(Node n1, Node n2) {
         return (n1.isWirelessEnabled() && n2.isWirelessEnabled()
-                && n1.distance(n2) < n1.getCommunicationRange());
+                && n1.distance(n2) <= n1.getCommunicationRange());
     }
 }
---
+++ lib/jbotsim-core/src/test/java/jbotsim/LinkResolverTest.java
@@ -0,0 +1,137 @@
+package jbotsim;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkResolverTest {
+
+    public static final Point POINT_ORIGIN = new Point(0, 0);
+    private LinkResolver resolver;
+    private Node n2;
+    private Node n1;
+
+    @BeforeEach
+    void setUp() {
+        resolver = new LinkResolver();
+        n1 = createNode(POINT_ORIGIN, 20);
+        n2 = createNode(new Point(10, 10), 5);
+    }
+
+    private Node createNode(Point location, double commRange) {
+        Node node = new Node();
+        node.setCommunicationRange(commRange);
+        node.setLocation(location);
+        return node;
+    }
+
+    //region isHeardBy
+
+    private void testHeardBy(Node n1, Node n2) {
+        boolean heard = resolver.isHeardBy(n1, n2);
+        assertTrue(heard);
+    }
+
+    private void testNotHeardBy(Node n1, Node n2) {
+        boolean heard = resolver.isHeardBy(n1, n2);
+        assertFalse(heard);
+    }
+
+    //region Basic behavior
+    @Test
+    void isHeardBy_properNodesInRange_heard() {
+        testHeardBy(n1, n2);
+    }
+
+    @Test
+    void isHeardBy_properNodesNotInRange_notHeard() {
+        testNotHeardBy(n2, n1);
+    }
+    //endregion
+
+    //region Incorrect parameters
+    @Test
+    void isHeardBy_n1Null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> {
+            resolver.isHeardBy(null, n2);
+        });
+    }
+    @Test
+    void isHeardBy_n2Null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> {
+            resolver.isHeardBy(n1, null);
+        });
+    }
+    @Test
+    void isHeardBy_n1n2Null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> {
+            resolver.isHeardBy(null, null);
+        });
+    }
+    //endregion
+
+
+    //region Wireless needed
+    @Test
+    void isHeardBy_n1NoWireless_notHeard() {
+        n1.setWirelessStatus(false);
+
+        testNotHeardBy(n1, n2);
+    }
+    @Test
+    void isHeardBy_n2NoWireless_notHeard() {
+        n2.setWirelessStatus(false);
+
+        testNotHeardBy(n1, n2);
+    }
+    @Test
+    void isHeardBy_n1n2NoWireless_notHeard() {
+        n1.setWirelessStatus(false);
+        n2.setWirelessStatus(false);
+
+        testNotHeardBy(n1, n2);
+    }
+    //endregion
+
+
+    //region Boundaries
+    @Test
+    void isHeardBy_n2AwayOnXaxis_notHeard() {
+        n2.setLocation(n1.communicationRange+1, 0, 0);
+
+        testNotHeardBy(n1, n2);
+    }
+    @Test
+    void isHeardBy_atLimintOnXaxis_heard() {
+        n2.setLocation(n1.communicationRange, 0, 0);
+        testHeardBy(n1, n2);
+    }
+    @Test
+    void isHeardBy_n2AwayOnYaxis_notHeard() {
+        n2.setLocation(0, n1.communicationRange+1, 0);
+
+        testNotHeardBy(n1, n2);
+    }
+    @Test
+    void isHeardBy_atLimitOnYaxis_heard() {
+        n2.setLocation(0, n1.communicationRange, 0);
+
+        testHeardBy(n1, n2);
+    }
+    @Test
+    void isHeardBy_n2AwayOnZaxis_notHeard() {
+        n2.setLocation(0, 0, n1.communicationRange+1);
+
+        testNotHeardBy(n1, n2);
+    }
+    @Test
+    void isHeardBy_atLimintOnZaxis_heard() {
+        n2.setLocation(0, 0, n1.communicationRange);
+
+        testHeardBy(n1, n2);
+    }
+
+    //endregion
+    //endregion
+}
