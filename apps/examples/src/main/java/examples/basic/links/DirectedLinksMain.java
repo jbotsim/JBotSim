e8c8ae6d544370624f99a22eb8ee5268f6662535
Remi Laplace <remi.laplace@labri.fr>
2018-12-17 14:45:14 +0100

    Chg: #71 - provide JDirectedLinkPainter

--- apps/examples/src/main/java/examples/basic/links/DirectedLinksMain.java
+++ apps/examples/src/main/java/examples/basic/links/DirectedLinksMain.java
@@ -1,11 +1,11 @@
 package examples.basic.links;
 
+import examples.basic.mobilebroadcast.MobileBroadcastNode;
 import io.jbotsim.core.Link;
 import io.jbotsim.core.Node;
 import io.jbotsim.core.Topology;
 import io.jbotsim.ui.JViewer;
-
-import java.util.List;
+import io.jbotsim.ui.painting.JDirectedLinkPainter;
 
 /**
  * This examples adds a bunch of random nodes and some directed links between some of them.
@@ -13,19 +13,21 @@ import java.util.List;
  */
 public class DirectedLinksMain {
 
-    public static final int MAX_NODE_ID = 50;
+    public static final int MAX_NODE_ID = 20;
+    protected static final int NB_LINKS = 15;
 
     public static void main(String[] args) {
         Topology tp = new Topology();
 
-        tp.setDefaultNodeModel(Node.class);
+        tp.setOrientation(Link.Orientation.DIRECTED);
         tp.disableWireless();
 
         deployRandomNodes(tp, MAX_NODE_ID);
 
-        addRandomDirectedLinks(tp,5, MAX_NODE_ID);
+        addRandomDirectedLinks(tp, NB_LINKS, MAX_NODE_ID);
 
-        new JViewer(tp);
+        JViewer viewer = new JViewer(tp);
+        viewer.getJTopology().setDefaultLinkPainter(new JDirectedLinkPainter());
         tp.start();
 
     }
@@ -38,23 +40,15 @@ public class DirectedLinksMain {
      */
     private static void deployRandomNodes(Topology tp, int maxNodeId) {
         for (int i = 0; i < maxNodeId; i++){
-            Node node = new Node();
-            node.setLocation(getRandomX(tp), getRandomY(tp));
+            Node node = new MobileBroadcastNode();
             node.setID(i);
-            tp.addNode(node);
+            tp.addNode(-1, -1, node);
         }
-    }
-
-    private static double getRandomX(Topology tp) {
-        return Math.random() * tp.getWidth();
-    }
-    private static double getRandomY(Topology tp) {
-        return Math.random() * tp.getHeight();
     }
 
     /**
      * Randomly add directed {@link Link}s between existing {@link Node}s in the {@link Topology}.
-     * 
+     *
      * @param tp the {@link Topology}
      * @param nbLinks the number of links to add
      * @param maxNodeId the upper bound of {@link Node} identifiers available in the {@link Topology}
@@ -72,14 +66,14 @@ public class DirectedLinksMain {
     }
 
     private static void addDirectedLink(Topology tp, Node from, Node to) {
-        tp.addLink(new Link(from, to, Link.Type.DIRECTED, Link.Mode.WIRED));
+        tp.addLink(new Link(from, to, Link.Orientation.DIRECTED, Link.Mode.WIRED));
     }
 
     private static boolean isFitForNewDirectedLink(Topology tp, Node from, Node to) {
         if(from == to)
             return false;
 
-        if(tp.getLink(from, to, true) != null)
+        if(tp.getLink(from, to, Link.Orientation.DIRECTED) != null)
             return false;
 
         return true;
