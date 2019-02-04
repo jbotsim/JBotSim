a26417483cea987e8e67806381217acf7e0716bc
Gerald Point <pictavien@free.fr>
2019-02-04 11:23:15 +0100

    Chg: #40 - add a nodeModel parameter to each generateX functions

--- lib/jbotsim-extras-common/src/main/java/io/jbotsim/topology/TopologyGenerator.java
+++ lib/jbotsim-extras-common/src/main/java/io/jbotsim/topology/TopologyGenerator.java
@@ -3,7 +3,13 @@ package io.jbotsim.topology;
 import io.jbotsim.core.Topology;
 
 public class TopologyGenerator {
+    public static final String DEFAULT_NODE_MODEL = "default";
+    
     public static void generateLine(Topology tp, int order){
+        generateLine(tp, order, DEFAULT_NODE_MODEL);
+    }
+
+    public static void generateLine(Topology tp, int order, String nodeModel){
         TopologyGeneratorFactory gf = new TopologyGeneratorFactory();
         gf.setAbsoluteCoords(true);
         gf.setX(50);
@@ -11,14 +17,24 @@ public class TopologyGenerator {
         gf.setWidth(tp.getWidth());
         gf.setHeight(tp.getHeight());
         gf.setWired(false);
-        gf.setNodeClass(tp.getNodeModel("default"));
+        gf.setNodeClass(tp.getNodeModel(nodeModel));
         gf.setOrder (order);
         gf.newHorizontalLine().generate(tp);
     }
+
     public static void generateRing(Topology topology, int nbNodes) {
-        generateRing(topology, nbNodes, false);
+        generateRing(topology, nbNodes, false, DEFAULT_NODE_MODEL);
     }
-    public static void generateRing(Topology topology, int nbNodes, boolean directed){
+
+    public static void generateRing(Topology topology, int nbNodes, String nodeModel) {
+        generateRing(topology, nbNodes, false, nodeModel);
+    }
+
+    public static void generateRing(Topology topology, int nbNodes, boolean directed) {
+        generateRing(topology, nbNodes, directed, DEFAULT_NODE_MODEL);
+    }
+
+    public static void generateRing(Topology topology, int nbNodes, boolean directed, String nodeModel) {
         TopologyGeneratorFactory gf = new TopologyGeneratorFactory();
         gf.setAbsoluteCoords(true);
         gf.setX(150);
@@ -27,14 +43,24 @@ public class TopologyGenerator {
         gf.setHeight(200.0);
         gf.setDirected(directed);
         gf.setWired(true);
-        gf.setNodeClass(topology.getNodeModel("default"));
+        gf.setNodeClass(topology.getNodeModel(nodeModel));
         gf.setOrder (nbNodes);
         gf.newRing().generate(topology);
     }
-    public static void generateGrid(Topology tp, int order){
-        generateGrid(tp, order, order);
+
+    public static void generateGrid(Topology tp, int order) {
+        generateGrid(tp, order, DEFAULT_NODE_MODEL);
     }
+    
+    public static void generateGrid(Topology tp, int order, String nodeModel){
+        generateGrid(tp, order, order, nodeModel);
+    }
+    
     public static void generateGrid(Topology tp, int orderX, int orderY){
+        generateGrid(tp, orderX, orderY, DEFAULT_NODE_MODEL);
+    }
+
+    public static void generateGrid(Topology tp, int orderX, int orderY, String nodeModel){
         TopologyGeneratorFactory gf = new TopologyGeneratorFactory();
         gf.setAbsoluteCoords(true);
         gf.setX(50);
@@ -42,11 +68,15 @@ public class TopologyGenerator {
         gf.setWidth(tp.getWidth()-50);
         gf.setHeight(tp.getWidth()-50);
         gf.setWired(false);
-        gf.setNodeClass(tp.getNodeModel("default"));
+        gf.setNodeClass(tp.getNodeModel(nodeModel));
         gf.newGrid(orderX, orderY).generate(tp);
 
     }
-    public static void generateTorus(Topology tp, int order){
+    public static void generateTorus(Topology tp, int order) {
+        generateTorus(tp, order, DEFAULT_NODE_MODEL);
+    }
+
+    public static void generateTorus(Topology tp, int order, String nodeModel){
         TopologyGeneratorFactory gf = new TopologyGeneratorFactory();
         gf.setAbsoluteCoords(true);
         gf.setX(50);
@@ -54,10 +84,14 @@ public class TopologyGenerator {
         gf.setWidth(tp.getWidth()-50);
         gf.setHeight(tp.getWidth()-50);
         gf.setWired(false);
-        gf.setNodeClass(tp.getNodeModel("default"));
+        gf.setNodeClass(tp.getNodeModel(nodeModel));
         gf.newTorus(order, order).generate(tp);
     }
     public static void generateKN(Topology topology, int nbNodes){
+        generateKN(topology, nbNodes, DEFAULT_NODE_MODEL);
+    }
+
+    public static void generateKN(Topology topology, int nbNodes, String nodeModel){
         TopologyGeneratorFactory gf = new TopologyGeneratorFactory();
         gf.setAbsoluteCoords(true);
         gf.setX(150);
@@ -65,7 +99,7 @@ public class TopologyGenerator {
         gf.setWidth(200);
         gf.setHeight(200.0);
         gf.setWired(false);
-        gf.setNodeClass(topology.getNodeModel("default"));
+        gf.setNodeClass(topology.getNodeModel(nodeModel));
         gf.setOrder (nbNodes);
         gf.newKN().generate(topology);
     }
