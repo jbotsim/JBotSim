1296f776e0033b2016a9125092929030a02d520d
Gerald Point <gerald.point@labri.fr>
2019-03-14 14:52:54 +0100

    Chg: #48 - implement DOT format export.

--- lib/jbotsim-serialization-common/src/main/java/io/jbotsim/io/format/dot/DotTopologySerializer.java
+++ lib/jbotsim-serialization-common/src/main/java/io/jbotsim/io/format/dot/DotTopologySerializer.java
@@ -24,84 +24,209 @@ import com.paypal.digraph.parser.GraphEdge;
 import com.paypal.digraph.parser.GraphNode;
 import com.paypal.digraph.parser.GraphParser;
 
-import io.jbotsim.core.Color;
-import io.jbotsim.core.Link;
-import io.jbotsim.core.Node;
-import io.jbotsim.core.Topology;
+import io.jbotsim.core.*;
 import io.jbotsim.io.TopologySerializer;
 
 import java.io.ByteArrayInputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DotTopologySerializer implements TopologySerializer {
+    private static Map<String, Color> NAMES_TO_COLORS;
+    private static Map<Color, String> COLORS_TO_NAMES;
+
+    static {
+        buildColorMaps();
+    }
+
     private double scale;
     private int margin;
+    private boolean reorganize;
+
+    private static final String JBOTSIM_ATTR_NODE_CLASS = "jbotsimNodeClass";
+    private static final String JBOTSIM_ATTR_IS_WIRELESS = "jbotsimIsWireless";
+    private static final String JBOTSIM_ATTR_IS_DIRECTED = "jbotsimIsDirected";
+
+    public static final double DEFAULT_SCALE = 2.0;
+    public static final int DEFAULT_MARGIN = 50;
 
     public DotTopologySerializer() {
-        this(2, 50);
+        this(DEFAULT_SCALE, DEFAULT_MARGIN);
+    }
+
+    public DotTopologySerializer(boolean reorganize) {
+        this(DEFAULT_SCALE, DEFAULT_MARGIN, reorganize);
+    }
+
+    public DotTopologySerializer(double scale, int margin, boolean reorganize) {
+        this.scale = scale;
+        this.margin = margin;
+        this.reorganize = reorganize;
     }
 
     public DotTopologySerializer(double scale, int margin) {
-        this.scale = scale;
-        this.margin = margin;
+        this(scale, margin, true);
     }
 
     @Override
     public void importFromString(Topology topology, String data) {
         topology.disableWireless();
-
         GraphParser parser = new GraphParser(new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8)));
         Map<String, GraphNode> nodes = parser.getNodes();
-        if(nodes.isEmpty())
+        if (nodes.isEmpty())
             return;
 
         Map<String, GraphEdge> edges = parser.getEdges();
 
         makeTopologyFromGraph(nodes, edges, topology);
-        organize(topology, scale, margin);
+        if (reorganize)
+            organize(topology, scale, margin);
     }
 
     @Override
     public String exportToString(Topology topology) {
-        return null;
+        boolean resume = false;
+        if (topology.isRunning()) {
+            topology.pause();
+            resume = true;
+        }
+        String edgeOp;
+        StringWriter str = new StringWriter();
+        PrintWriter out = new PrintWriter(str);
+        if (topology.hasDirectedLinks()) {
+            edgeOp = " -> ";
+            out.print("digraph");
+        } else {
+            edgeOp = " -- ";
+            out.print("graph");
+        }
+        out.println('{');
+        for (Node n : topology.getNodes()) {
+            Color c = n.getColor();
+            Point pos = n.getLocation();
+            out.print("  " + n.getID() + " [");
+            if (c != null) {
+                out.print("style = \"filled\", fillcolor = \"" + getStringForColor(c) + "\", ");
+            }
+            out.print("pos = \"" + pos.getX() + "," + pos.getY() + "!\", ");
+            out.print(JBOTSIM_ATTR_NODE_CLASS + " = \"" + n.getClass().getName() + "\", ");
+            out.print("label = \"" + n.getID() + "\"");
+            out.println("];");
+        }
+
+        for (int i = 0; i < 2; i++) {
+            List<Link> links = topology.getLinks(i == 0);
+            for (Link l : links) {
+                int src = 0;
+                if (!l.isDirected() && l.endpoint(1).getID() < l.endpoint(0).getID())
+                    src = 1;
+                out.print(l.endpoint(src).getID());
+                out.print(edgeOp);
+                out.print(l.endpoint((src + 1) % 2));
+                out.print(" [");
+                Integer w = l.getWidth();
+                if (w.equals(Link.DEFAULT_WIDTH)) {
+                    out.print("width = \"" + w + "\", ");
+                }
+                Color c = l.getColor();
+                if (!Link.DEFAULT_COLOR.equals(c)) {
+                    out.print("color = \"" + getStringForColor(c) + "\", ");
+                }
+                out.print(JBOTSIM_ATTR_IS_WIRELESS + " = \"" + (l.isWireless() ? 1 : 0) + "\", ");
+                out.print(JBOTSIM_ATTR_IS_DIRECTED + " = \"" + (l.isDirected() ? 1 : 0) + "\", ");
+                out.print("style = \"" + (l.isWireless() ? "dashed" : "solid") + "\"");
+                out.println("];");
+            }
+        }
+        out.println('}');
+        out.flush();
+        if (resume)
+            topology.resume();
+        return str.getBuffer().toString();
     }
 
-    private void makeTopologyFromGraph(Map<String,GraphNode> nodes, Map<String,GraphEdge> edges, Topology tp) {
-        HashMap<GraphNode,Node> nodeTable = new HashMap<>();
+    private void makeTopologyFromGraph(Map<String, GraphNode> nodes, Map<String, GraphEdge> edges, Topology tp) {
+        HashMap<GraphNode, Node> nodeTable = new HashMap<>();
+        boolean enableWireless = false;
 
-        for(String nname : nodes.keySet()) {
+        for (String nname : nodes.keySet()) {
+            Node node;
             GraphNode gn = nodes.get(nname);
-            Node node = tp.newInstanceOfModel("default");
+            String className = (String) gn.getAttribute(JBOTSIM_ATTR_NODE_CLASS);
+            if (className == null) {
+                node = tp.newInstanceOfModel(Topology.DEFAULT_NODE_MODEL_NAME);
+            } else {
+                try {
+                    Class<? extends Node> nodeClass = Class.forName(className).asSubclass(Node.class);
+                    node = nodeClass.newInstance();
+                } catch (Exception e) {
+                    node = tp.newInstanceOfModel(Topology.DEFAULT_NODE_MODEL_NAME);
+                }
+            }
             extractAttributes(gn, node);
+            try {
+                node.setID(Integer.parseInt(gn.getId()));
+            } catch (NumberFormatException e) {
+                node.setLabel(gn.getId());
+            }
             tp.addNode(node);
             nodeTable.put(gn, node);
         }
-        Link.Type linkType = Link.Type.UNDIRECTED;
 
-        for(GraphEdge edge : edges.values()) {
+        for (GraphEdge edge : edges.values()) {
+            String isWireless = (String) edge.getAttribute(JBOTSIM_ATTR_IS_WIRELESS);
+            if (isWireless != null && Integer.parseInt(isWireless) == 1) {
+                enableWireless = true;
+                continue;
+            }
+
             GraphNode gn1 = edge.getNode1();
             Node src = nodeTable.get(gn1);
             GraphNode gn2 = edge.getNode2();
             Node tgt = nodeTable.get(gn2);
+
+            Link.Type linkType = Link.Type.UNDIRECTED;
+            String isDirected = (String) edge.getAttribute(JBOTSIM_ATTR_IS_DIRECTED);
+            if (isDirected != null && Integer.parseInt(isDirected) == 1) {
+                linkType = Link.Type.DIRECTED;
+            }
+
+            if (linkType.equals(Link.Type.UNDIRECTED)) {
+                if (src.getNeighbors().contains(tgt))
+                    continue;
+
+                if (src.getID() > tgt.getID()) {
+                    Node tmp = src;
+                    src = tgt;
+                    tgt = tmp;
+                }
+            } else if (src.getOutLinkTo(tgt) != null) {
+                continue;
+            }
             Link l = new Link(src, tgt, linkType);
             extractAttributes(edge, l);
             tp.addLink(l);
         }
+        tp.setWirelessStatus(enableWireless);
     }
 
     private void extractAttributes(GraphNode mn, Node node) {
         String pos = (String) mn.getAttribute("pos");
         if (pos != null) {
-            String[] xy = pos.split(",");
+            String[] xy = pos.replace("!", "").split(",");
             node.setLocation(Double.valueOf(xy[0]), Double.valueOf(xy[1]));
         }
 
         String c = (String) mn.getAttribute("color");
         if (c != null) {
             try {
-                node.setColor(Color.decode(c.substring(0)));
+                node.setColor(getColorFromString(c));
             } catch (NumberFormatException e) {
 
             }
@@ -109,7 +234,7 @@ public class DotTopologySerializer implements TopologySerializer {
         c = (String) mn.getAttribute("fillcolor");
         if (c != null) {
             try {
-                node.setColor(Color.decode(c.substring(0)));
+                node.setColor(getColorFromString(c));
             } catch (NumberFormatException e) {
 
             }
@@ -120,7 +245,7 @@ public class DotTopologySerializer implements TopologySerializer {
         String c = (String) ml.getAttribute("color");
         if (c != null) {
             try {
-                l.setColor(Color.decode(c.substring(0)));
+                l.setColor(getColorFromString(c));
             } catch (NumberFormatException e) {
 
             }
@@ -135,8 +260,8 @@ public class DotTopologySerializer implements TopologySerializer {
         }
     }
 
-    public static void organize(Topology tp, double scale, int margin){
-        if(tp.getNodes().isEmpty())
+    public static void organize(Topology tp, double scale, int margin) {
+        if (tp.getNodes().isEmpty())
             return;
 
         // Applies Scale
@@ -148,18 +273,19 @@ public class DotTopologySerializer implements TopologySerializer {
         double minY = Integer.MAX_VALUE;
         double maxX = 0;
         double maxY = 0;
-        for (Node node : tp.getNodes()){
-            if (node.getX() < minX)
-                minX = node.getX();
-            if (node.getX() > maxX)
-                maxX = node.getX();
-            if (node.getY() < minY)
-                minY = node.getY();
-            if (node.getY() > maxY)
-                maxY = node.getY();
+        for (Node node : tp.getNodes()) {
+            int sz = node.getIconSize();
+            if (node.getX() - sz < minX)
+                minX = node.getX() - sz;
+            if (node.getX() + sz > maxX)
+                maxX = node.getX() + sz;
+            if (node.getY() - sz < minY)
+                minY = node.getY() - sz;
+            if (node.getY() + sz > maxY)
+                maxY = node.getY() + sz;
         }
-        double width = (maxX - minX) + 2*margin;
-        double height = (maxY - minY) + 2*margin;
+        double width = (maxX - minX) + 2 * margin;
+        double height = (maxY - minY) + 2 * margin;
         tp.setDimensions((int) width, (int) height);
         double xshift = margin - minX;
         double yshift = margin - minY;
@@ -169,5 +295,45 @@ public class DotTopologySerializer implements TopologySerializer {
         // Flips Y-coordinate
         for (Node node : tp.getNodes())
             node.setLocation(node.getX(), height - node.getY());
+
+        tp.setCommunicationRange(tp.getCommunicationRange() * scale);
+        tp.setSensingRange(tp.getSensingRange() * scale);
+    }
+
+    private static Color getColorFromString(String s) {
+        Color result = NAMES_TO_COLORS.get(s);
+        if (result == null) {
+            result = Color.decode(s);
+        }
+        return result;
+    }
+
+    private static String getStringForColor(Color c) {
+        String result = COLORS_TO_NAMES.get(c);
+        if (result == null) {
+            result = String.format("#%02x%02x%02x", c.getRed(), c.getGreen(), c.getBlue());
+        }
+        return result;
+    }
+
+    private static final void buildColorMaps() {
+        NAMES_TO_COLORS = new HashMap<>();
+        COLORS_TO_NAMES = new HashMap<>();
+
+        for (Field field : Color.class.getDeclaredFields()) {
+            if (Color.class.equals(field.getType())) {
+                int modifiers = field.getModifiers();
+                if (Modifier.isPublic(modifiers) && Modifier.isStatic(modifiers) && Modifier.isFinal(modifiers)) {
+                    String fieldName = field.getName();
+                    try {
+                        Color c = (Color) field.get(Color.class);
+                        NAMES_TO_COLORS.put(fieldName, c);
+                        COLORS_TO_NAMES.put(c, fieldName);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("buildColorMap: " + e.getMessage());
+                    }
+                }
+            }
+        }
     }
 }
