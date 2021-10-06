a5f98bfbe558bdac277f507ac82bf71fc8b68143
Arnaud Casteigts <arnaud.casteigts@labri.fr>
2021-10-06 19:21:11 +0200

    Chg: #105 - Other improvements of Message.toString()

--- lib/jbotsim-core/src/main/java/io/jbotsim/core/Message.java
+++ lib/jbotsim-core/src/main/java/io/jbotsim/core/Message.java
@@ -165,6 +165,7 @@ public final class Message extends Properties {
 
     @Override
     public String toString() {
-        return sender + " -> " + destination + ": content=" + content + ", flag=" + flag;
+        return sender.getID() + " -> " + destination.getID()
+                + ": content=" + content + ", flag=" + flag;
     }
 }
